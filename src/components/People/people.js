018251afc930cb1ddc5ef33a5291b104aa71abc2
Alan Guerrero <alan.guerrero@gmail.com>
2018-09-24 15:41:37 -0400

    adding vehicle data to a person

--- src/components/People/people.js
+++ src/components/People/people.js
@@ -8,75 +8,76 @@ const animation = 'flipInX';
 const animationDelay = 25; // in ms
 
 export default Vue.extend({
-  template,
+    template,
 
-  data() {
-    return {
-      peopleFilter: '',
-      people: []
-    };
-  },
-
-  computed: {
-    filteredPeople() {
-      return this.people.filter((person) => person.name.toLowerCase().indexOf(this.peopleFilter.toLowerCase()) !== -1);
+    data() {
+        return {
+            peopleFilter: '',
+            people: []
+        };
     },
 
-    peopleSummary() {
-      let sums  =this.people.reduce((last, p, i) => {
-        let next = Object.assign(last, { count: i + 1 })
-        next.sumHeight += isNaN(p.height) ? 0 : Number(p.height);
-        next.sumMass += isNaN(p.mass) ? 0 : Number(p.mass);
-        return next;
-      },{
-        sumHeight: 0,
-        sumMass: 0,
-        count: 0,
-        gender: {},
-      });
+    computed: {
+        filteredPeople() {
+            return this.people.filter((person) => person.name.toLowerCase().indexOf(this.peopleFilter.toLowerCase()) !== -1);
+        },
 
-      sums.avgHeight = d3.format(",.1f")(sums.sumHeight / sums.count) + ' cm'
-      sums.avgMass = d3.format(",.1f")(sums.sumMass / sums.count) + ' kg'
-      return sums
+        peopleSummary() {
+            let sums = this.people.reduce((last, p, i) => {
+                let next = Object.assign(last, { count: i + 1 })
+                next.sumHeight += isNaN(p.height) ? 0 : Number(p.height);
+                next.sumMass += isNaN(p.mass) ? 0 : Number(p.mass);
+                return next;
+            }, {
+                    sumHeight: 0,
+                    sumMass: 0,
+                    count: 0,
+                    gender: {},
+                    vehicles: [],
+                });
+
+            sums.avgHeight = d3.format(",.1f")(sums.sumHeight / sums.count) + ' cm'
+            sums.avgMass = d3.format(",.1f")(sums.sumMass / sums.count) + ' kg'
+            return sums
+        }
+    },
+
+    created() {
+        this.fetchPeople();
+    },
+
+    methods: {
+        fetchPeople(page) {
+            if (!page) {
+                this.people = [];
+            }
+            return starWarsResource.get('/people/' + (page ? `?page=${page}` : ''))
+                .then((response) => {
+                    let newPeople = response.data.results.map(p => Object.assign(p, { id: p.url.slice(-2).slice(0, 1) }))
+                    this.people = this.people.concat(newPeople);
+                    if (response.data.next) {
+                        let nextPage = page ? ++page : 2;
+                        return this.fetchPeople(nextPage)
+                    }
+                })
+                .catch((errorResponse) => {
+                    // Handle error...
+                    console.log('API responded with:', errorResponse);
+                });
+        },
+
+        // Methods for transitions
+        handleBeforeEnter(el) {
+            el.style.opacity = 0;
+            el.classList.add('animated');
+        },
+
+        handleEnter(el) {
+            const delay = el.dataset.index * animationDelay;
+            setTimeout(() => {
+                el.style.opacity = 1;
+                el.classList.add(animation);
+            }, delay);
+        }
     }
-  },
-
-  created(){
-    this.fetchPeople();
-  },
-
-  methods: {
-    fetchPeople(page){
-      if (!page) {
-        this.people = [];
-      }
-      return starWarsResource.get('/people/' + (page ? `?page=${page}`: ''))
-        .then((response) => {
-          let newPeople = response.data.results.map(p => Object.assign(p, {id: p.url.slice(-2).slice(0,1)}))
-          this.people = this.people.concat(newPeople);
-          if (response.data.next) {
-            let nextPage = page ? ++page : 2;
-            return this.fetchPeople(nextPage)
-          }
-        })
-        .catch((errorResponse) => {
-          // Handle error...
-          console.log('API responded with:', errorResponse);
-        });
-    },
-
-    // Methods for transitions
-    handleBeforeEnter(el) {
-      el.style.opacity = 0;
-      el.classList.add('animated');
-    },
-
-    handleEnter(el) {
-      const delay = el.dataset.index * animationDelay;
-      setTimeout(() => {
-        el.style.opacity = 1;
-        el.classList.add(animation);
-      }, delay);
-    }
-  }
 });
